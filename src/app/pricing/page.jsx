c9d91e1937d4906e0f60d80223c7096f284e0003
Syed Farhad Reza <syedfarhad71@gmail.com>
2026-06-19 20:23:05 +0600

    first commit

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,10 @@
+
+const pricingpage = () => {
+    return (
+        <div>
+            <h1 className="text-green-600 text-center text-4xl font-bold m-5 p-5 border-2">We are updateing pricing page ...</h1>
+        </div>
+    );
+};
+
+export default pricingpage;
